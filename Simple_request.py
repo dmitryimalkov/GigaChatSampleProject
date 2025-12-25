b56b8b7111c440b827ecbfa57fb1c85461baa32b
Dmitry Malkov <59419740+dmitryimalkov@users.noreply.github.com>
2025-12-25 13:27:53 +0300

    Update Simple_request.py
    Authorization key

--- Simple_request.py
+++ Simple_request.py
@@ -1,7 +1,7 @@
 from gigachat import GigaChat
 
 # Укажите ключ авторизации, полученный в личном кабинете, в интерфейсе проекта GigaChat API
-with GigaChat(credentials="ZDNmYjJiY2UtY2E2Ni00YmRiLWFkNjktMmRiNzM3ZTRhZjI0OjU3Y2FiYTE0LTBlNjItNDU3NC1hMmVkLTRhYjJlZTI1NDEzNw==", verify_ssl_certs=False) as giga:
+with GigaChat(credentials="Authorization key", verify_ssl_certs=False) as giga:
     response = giga.chat("2+2")
 # Ответ по доступным моделям
     print(response.choices[0].message.content)
@@ -10,4 +10,5 @@ with GigaChat(credentials="ZDNmYjJiY2UtY2E2Ni00YmRiLWFkNjktMmRiNzM3ZTRhZjI0OjU3Y
 # Получение токена
     response = giga.get_token()
     print(response)
+
 
